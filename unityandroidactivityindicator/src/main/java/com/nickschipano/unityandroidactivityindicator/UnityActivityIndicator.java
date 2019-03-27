2eefd82996f41057eaa23352f2c914e8ef25dd46
Nick Schipano <nick@firstfoundry.co>
2019-03-26 17:54:46 -0700

    Added logic to find Unity's ProgressBar widget and set its gravity to CENTER

--- unityandroidactivityindicator/src/main/java/com/nickschipano/unityandroidactivityindicator/UnityActivityIndicator.java
+++ unityandroidactivityindicator/src/main/java/com/nickschipano/unityandroidactivityindicator/UnityActivityIndicator.java
@@ -1,4 +1,80 @@
 package com.nickschipano.unityandroidactivityindicator;
 
-public class UnityActivityIndicator {
+import android.content.Context;
+import android.view.*;
+import android.app.Activity;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.util.Log;
+import android.app.Activity;
+import java.lang.Runnable;
+
+public class UnityActivityIndicator
+{
+    private Context _context;
+    private static String tag = "UnityActivityIndicator";
+
+    public UnityActivityIndicator(Context context)
+    {
+        // context passed in to ctor should point to the com.unity3d.player.UnityPlayer activity
+        this._context = context;
+    }
+
+    public void MoveToCenterOfScreen()
+    {
+        // This needs to happen on the ui thread because that's
+        // where Unity instantiates the layout.
+        final Activity activity = (Activity)_context;
+        activity.runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                View view = activity.findViewById(android.R.id.content);
+                ViewGroup vg = (ViewGroup)view;
+                CenterChildProgressBar(vg);
+            }
+        });
+    }
+
+    private void CenterChildProgressBar(ViewGroup vg)
+    {
+        final int count = vg.getChildCount();
+        for (int i=0; i<count; i++)
+        {
+            View v = vg.getChildAt(i);
+
+            if (v instanceof ProgressBar)
+            {
+                LayoutParams lp = v.getLayoutParams();
+                Log.i(tag, "Found ProgressBar!");
+
+                // I'm sure there's a more elegant way to do this
+                if (lp instanceof FrameLayout.LayoutParams)
+                {
+                    FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) v.getLayoutParams();
+                    params.gravity = Gravity.CENTER;
+                    v.setLayoutParams(params);
+                }
+                else if (lp instanceof LinearLayout.LayoutParams)
+                {
+                    LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) v.getLayoutParams();
+                    params.gravity = Gravity.CENTER;
+                    v.setLayoutParams(params);
+                }
+                else {
+                    // shrug!
+                    Log.w(tag, "Unknown layout for ProgressBar!");
+                }
+                break;
+            }
+            else if (v instanceof ViewGroup)
+            {
+                // we found a ViewGroup, look at its children too
+                CenterChildProgressBar((ViewGroup)v);
+            }
+        }
+    }
 }
